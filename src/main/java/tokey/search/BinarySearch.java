e8a57f14e4625318bf0c39a4edc25ba4fdd1ce98
root <1446764177@qq.com>
2021-03-24 16:03:48 +0800

    project creation

---
+++ src/main/java/tokey/search/BinarySearch.java
@@ -0,0 +1,18 @@
+package tokey.search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+
+
+    }
+
+   public static  void   swap(int [] A  ,int a , int b){
+            int temp = A[a];
+            A[a] = A[b];
+            A[b] = temp;
+
+   }
+
+
+}
