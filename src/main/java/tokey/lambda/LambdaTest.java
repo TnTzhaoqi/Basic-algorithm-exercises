e09e705cc7ae2e75649ddb4b573fbbe8d5aadcfa
root <1446764177@qq.com>
2022-03-31 14:20:15 +0800

    3.13 add lambdatest

---
+++ src/main/java/tokey/lambda/LambdaTest.java
@@ -0,0 +1,16 @@
+package tokey.lambda;
+
+import org.junit.Test;
+
+public class LambdaTest {
+    @Test
+    public void test(){
+        Operation sum1 = (int x ,int y) ->(x+y);
+        Operation sum2 = (x, y) -> x*y;
+
+
+
+
+
+    }
+}
